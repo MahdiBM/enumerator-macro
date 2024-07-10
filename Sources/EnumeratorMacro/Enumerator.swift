a1ae02e7516e40157d21befb20686e4cb4f39adb
MahdiBM <github@mahdibm.com>
2024-07-10 22:25:51 +0330

    fixes

--- Sources/EnumeratorMacro/Enumerator.swift
+++ Sources/EnumeratorMacro/Enumerator.swift
@@ -2,97 +2,51 @@
 import SwiftSyntaxMacros
 
 @attached(member, names: arbitrary)
-public macro Enumerator(_ templates: String...) = #externalMacro(
+public macro Enumerator(_ templates: StaticString...) = #externalMacro(
     module: "EnumeratorMacroImpl",
     type: "EnumeratorMacroType"
 )
 
-//@attached(member, names: arbitrary)
-//macro CreateSubtype(
-//    _ template: String = """
-//    enum Subtype: String {
-//        {{#cases}}
-//        case {{name}}
-//        {{/cases}}
-//    }
-//    """
-//) = #externalMacro(
-//    module: "EnumeratorMacroImpl",
-//    type: "EnumeratorMacroType"
-//)
-//
-//@CreateSubtype()
-//enum TestEnum2 {
-//    case a(value: String)
-//    case b
-//    case f
-//    case testCase(testValue: String)
-//
-//    func dso() {
-//        let a = Subtype.a
-//    }
-//}
-//
-//@CreateSubtype()
-//enum TestEnumd2 {
-//    case a(value: String)
-//    case b
-//    case f
-//    case testCase(testValue: String)
-//}
+@attached(member, names: arbitrary)
+macro CreateSubtype(
+    _ templates: String = """
+    enum Subtype: String {
+        {{#cases}}
+        case {{name}}
+        {{/cases}}
+    }
+    """
+) = #externalMacro(
+    module: "EnumeratorMacroImpl",
+    type: "EnumeratorMacroType"
+)
 
 @Enumerator("""
 enum Subtype: String {
-{{#cases}}
-case {{name}}
-{{/cases}}
-}
-""",
-"""
-var subtype: Subtype {
-    switch self {
     {{#cases}}
-    case .{{name}}:
-        return .{{name}}
+    case {{name}}
     {{/cases}}
-    }
 }
-""",
-"""
-var parameterNames: [String] {
-    switch self {
-    {{#cases}}
-    case .{{name}}:
-        return [
-{{^parameters}}
-"empty"
-{{/parameters}}
-{{#parameters}}
-"{{snakeCased(name)}}"
-{{/parameters}}
-        ]
-    {{/cases}}
-    }
-}
-""",
-"""
-{{#cases}}
-var is{{capitalized(name)}}: Bool {
-    switch self {
-    case .{{name}}:
-        return true
-    default:
-        return false
-    }
-}
-{{/cases}}
 """)
-enum TestEnum {
+enum TestEnum2 {
     case a(value: String)
     case b
+    case f
     case testCase(testValue: String)
 
-    func isTheSameCase(as other: Self) -> Bool {
-        self.subtype == other.subtype
+    func dso() {
+        let a = Subtype.a
     }
+}
+
+@CreateSubtype()
+enum TestEnumd2 {
+    case a(value: String)
+    case b
+    case f
+    case testCase(testValue: String)
+
+//    func dso() {
+//        let a = Subtype.a
+//    }
 }
